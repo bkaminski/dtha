5710ada765dfd5d17d769bd5db14715f5415a85d
Ben Kaminski <ben@benkaminski.com>
2020-11-18 10:51:20 -0500

    added envira gallery for homepage

--- front-page.php
+++ front-page.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-	
-
 	<header>
 		<?php get_template_part('parts/home', 'carousel'); ?>
 
--- parts/home-image-blocks.php
+++ parts/home-image-blocks.php
@@ -1,3 +1,4 @@
 <div class="image-blocks">
+	<?php echo do_shortcode('[envira-gallery slug="home-page-thumb-gallery"]'); ?>
 	
 </div>
